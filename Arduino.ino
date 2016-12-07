d98a784a3bb772cf9f91b6fd66be7a61fb6a91df
Mariana-Barros <msb4@cin.ufpe.br>
2016-12-07 02:37:29 -0300

    Versão final do Arduino do Dispositivo.

--- Arduino.ino
+++ Arduino.ino
@@ -1,38 +1,130 @@
+#include "TinyGPS++.h"
+#include <Wire.h>
+#include <OBD.h>
+#include <SoftwareSerial.h>
 #include <SPI.h>
 #include "RF24.h"
 #include <nRF24L01.h>
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
 #include <string.h>
-#include <Wire.h>
-#include <OBD.h>
-#include "TinyGPS++.h"
-#include "SoftwareSerial.h"
 
-#define TAM_ARRAY 10
-#define VEL_LIM 25 // Valor recebido do banco de dados
-
-COBD obd; /* for Model A (UART version) */
+TinyGPSPlus gps;
+COBD obd; 
 
 // Variáveis radio
 bool radioNumber = 1;
 //1 - Arduino;
 //0 - Galileo;
 RF24 radio(7,8);
-
-// Feedback
-const int buzzer = 9; //buzzer to arduino pin 9
-
-// GPS
-TinyGPSPlus gps;
-SoftwareSerial gps_serial(10, 11); //RX=pin 10, TX= pin 11
-
-// Rede RF
 const uint64_t wAddress[] = {0x7878787878LL, 0xB3B4B5B6CDF1, 0xB3B4B5B6CDLL, 0xB3B4B5B6F1LL};
 
 // Dados comunicação
-char data_send[45];
-char data_rcv[45];
+char data_send[150];
+char data_rcv[150];
+char instruction[20];
+char* str;
+
+int i = 0;
+int velocidade[] = {0,0,0,0,0,0,0,0,0,0};
+int vel = 20;
+double latitude = 0, longitude = 0;
+char* sptr;
+double lat_old;
+double lng_old;
+
+int cont = 0;
+
+char lat_st[10];
+char lon_st[10];
+
+int j = 0; // Sincronizar gps com rf
+int id = 2;
+int pkt = 0;
+
+
+ int risco;
+ int vel_limit;
+
+
+
+int LerVelocidade(){
+  int value;
+
+  if (obd.readPID(PID_SPEED, value)){
+    return value;
+  }
+  return 0;
+}
+
+void ImprimirDados(int vel, double lat, double lng){
+    Serial.print("Velocidade media = ");
+    Serial.println(vel);
+    Serial.print("Lat = ");
+    Serial.print(lat, 6);
+    Serial.print(" || Lng = ");
+    Serial.println(lng, 6);
+    Serial.println();
+}
+
+int Media(int vel[]){
+    int soma = 0, x;
+
+    for(x=0; x<10; x++){
+      soma += vel[x];
+    }
+    return ((int) (soma/10));
+}
+
+char* CriarString(char* txt,int vel, double lat, double lng){
+  char out1[11];
+  char out2[12];
+
+  if((lat == 0.0) && (lng == 0.0)) {
+    dtostrf(lat_old,9, 6, out1);
+    dtostrf(lng_old,10, 6, out2);
+  } else {
+    dtostrf(lat,9, 6, out1);
+    dtostrf(lng,10, 6, out2);
+
+    lat_old = lat;
+    lng_old  = lng;
+  }
+  sprintf(txt,"%d %s %s", vel, out1, out2);
+
+  return txt;
+}
+
+
+void setup()
+{
+  Serial.begin(9600);
+  Serial1.begin(9600);
+  Serial2.begin(9600);
+
+
+
+  sptr = (char*) malloc(36*sizeof(char));
+
+  //obd.begin();
+  //while (!obd.init());
+
+  // Inicializa os valores de latitude e longitude
+  latitude = 0;
+  longitude = 0;
+
+  //Inicializa o RF
+  radio.begin();
+  radio.setPALevel(RF24_PA_LOW);
+  radio.setChannel(108);          
+   
+  // Abre um canal de leitura e um de escrita para o RF
+  radio.openWritingPipe(wAddress[1]);
+  radio.openReadingPipe(1,wAddress[0]);
+
+  // Começa a ouvir pelo RF
+  radio.startListening();
+}
 
 //Funcao de escrever no RF
 void sendRF(){
@@ -70,81 +162,52 @@ boolean receiveRF(){
     return timeout;
 }
 
-void setup()
-{
-  Serial.begin(38400);
-  gps_serial.begin(9600);
-
-  // start communication with OBD-II adapter
-  obd.begin();
-  // initiate OBD-II connection until success
-  while (!obd.init());
-
-  // Inicializa o pino buzzer
-  pinMode(buzzer, OUTPUT); // Set buzzer - pin 9 as an output
-
-  //Inicializa o RF
-  radio.begin();
-  radio.setPALevel(RF24_PA_LOW);
-  radio.setChannel(108); 
-    // Abre um canal de leitura e um de escrita para o RF
-  radio.openWritingPipe(wAddress[1]);
-  radio.openReadingPipe(1,wAddress[0]);
-  // Começa a ouvir pelo RF
-  radio.startListening();
-}
-
 void loop()
 {
-  int i, soma = 0, velocidade;
-
-  // PEGAR DADOS DO OBD-II
-  for(i=0, i < TAM_ARRAY, i++){
-    delay(100);
-    // save SPEED in variable 'value', return true on success
-    if (obd.readPID(PID_SPEED, value)) {
-      soma = soma + value;
-                                                                                  Serial.print("Velocidade instantanea: ");
-                                                                                  Serial.println(value);
+  // PEGAR A LOCALIZAÇÃO
+  delay(40);
+  while(Serial2.available()){
+    if (gps.encode(Serial2.read())) {
+      j = 1;
+      latitude = gps.location.lat();
+      longitude = gps.location.lng();
     }
-
-    if(value > VEL_LIM) {
-        tone(buzzer, 1000); // Send 1KHz sound signal...
-      } else {
-        noTone(buzzer);     // Stop sound...
-      }
   }
-  velocidade = (int) soma/TAM_ARRAY;
-
-  // MÓDULO GPS PEGA OS DADOS
-  do {
-    if (gps.encode(gps_serial.read())) {
-      Serial.println("Quantidade de Satelites:");
-      Serial.println(gps.satellites.value());
-      Serial.println("Latitude:");
-      Serial.println(gps.location.lat(), 6);
-      Serial.println("Longitude:");
-      Serial.println(gps.location.lng(), 6);
-      Serial.println();
-    }
-  } while(!gps_serial.available());
-
-                                                                                  Serial.print("Velocidade média: ");
-                                                                                  Serial.println(velocidade);
-
-  // FORMAÇÃO DA STRING PARA ENVIAR PRA GALILEO
-
-  // ENVIO PARA GALILEO
-  //sprintf(data_send, "&lat=%s&lon=%s&vel=%d", lat, lon, velocidade);
-  //sendRF();
- 
-  // Fica esperando mensagem da Galileo
- // boolean timeout;
-  //timeout = receiveRF();
   
-  /*if (!timeout){
-      // Verifica a informação recebida
-      sscanf(data_rcv, "%d %s", &velocidade_rcv, instruction); // Pega mensagem que chegou e separa no id e na instrução
-  }*/
+  // PEGAR A VELOCIDADE
+  delay(40);
+  velocidade[i] = LerVelocidade();
 
+  // IMPRIMIR OS DADOS PARA VISUALIZAÇÃO (SERÁ TROCADO PELO MÓDULO GSM)
+  delay(20);
+  if(i == 9){
+    ImprimirDados(Media(velocidade),latitude,longitude);
+    str = CriarString(sptr,Media(velocidade),latitude,longitude);
+
+    if (j ==1) {
+        // Arduino manda mensagem para Galileo
+        strcpy(data_send, str);        
+        sendRF();
+       
+        // Fica esperando mensagem da Galileo
+        boolean timeout;
+        timeout = receiveRF();
+
+        sscanf(data_rcv, "%d %d", &risco, &vel_limit);
+        delay(100);
+        j = 0;
+      }
+      
+    i = 0;
+    latitude = 0;
+    longitude = 0;
+  } else{
+      Serial.print("Velocidade = ");
+      Serial.println(velocidade[i]);
+      i++;
+    }
+    delay(100);
+    cont++;
+
+    
 }
